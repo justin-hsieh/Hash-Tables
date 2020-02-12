5f01b1e4cdcd01ec2748ba8319962b58a4c3d548
Justin <showin@live.com>
2020-02-12 09:36:34 -0700

    update assignment

--- src/hashtable.py
+++ src/hashtable.py
@@ -54,11 +54,15 @@ class HashTable:
         index = self._hash_mod(key)
         current_value = self.storage[index]
 
+        # if there is a node that already exists with same key
         if current_value is not None:
             print("Warning", value, " Colliding ", current_value.value)
+
+            # create new link pair
             newLinkPair = LinkedPair(key, value)
             newLinkPair.next = self.storage[index]
             self.storage[index] = newLinkPair
+        # if node with same ke doesn't already exist, create link pair and add to storage
         else:
             self.storage[index] = LinkedPair(key, value)
 
@@ -73,8 +77,10 @@ class HashTable:
         Fill this in.
         '''
         index = self._hash_mod(key)
+        # if the key can't be found
         if self.storage[index] is None:
             print("The Key has not been found")
+        # set key in storage to None 
         else:
             self.storage[index] = None
 
@@ -90,12 +96,15 @@ class HashTable:
         index = self._hash_mod(key)
         current_key = self.storage[index]
         
+        # if node is found, verify it matches
         while current_key:
+            # if value does not match, move to next node and check again
             if current_key.key != key:
                 current_key = current_key.next
+            # if found, return value
             else:
                 return current_key.value
-
+        # return None if not found
         return None
 
 
@@ -106,8 +115,19 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        self.capacity *= 2
+        previous_storage = self.storage
+        self.storage = self.capacity * [None]
 
+        # for each previous key in the storage
+        for previous_keys in previous_storage:
+            # set current key to previous key
+            current_key = previous_keys
+            # if key exists
+            while current_key is not None:
+                # insert node and move onto next key
+                self.insert(current_key.key, current_key.value)
+                current_key = current_key.next
 
 
 if __name__ == "__main__":
